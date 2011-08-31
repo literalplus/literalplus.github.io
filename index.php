404fece399fb1f25e6a7c874cdbd961a4d0deab6
xxyy <xxyy598@gmail.com>
2011-08-31 22:24:56 +0200

    rev 2

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<head>
+<title>xxyy</title>
+</head>
+<body>
+Welcome to my github page. <br />
+<a href="http://xxyy.bplaced.net/">My Website</a><br />
+<?php
+echo date("Y-m-d");
+?>
+</body>
+</html>
